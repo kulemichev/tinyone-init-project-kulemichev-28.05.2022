8fc15ce5b5e432930217d44f1e5a1207f73b2aa0
Alex Kulemichev <alexey.kulemichev@gmail.com>
2022-08-14 17:31:37 +0300

    Html fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,90 +1,90 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  /* menu */
-  let $btnHamburger = $('.header-navbar__hamburger');
-  let $navbarList = $('.header-navbar__list');
-  let $window = $(window);
-  let $body = $('body');
-  let isShow = false;
-  let isStop = true;
-  /* menu */
+//   /* menu */
+//   let $btnHamburger = $('.header-navbar__hamburger');
+//   let $navbarList = $('.header-navbar__list');
+//   let $window = $(window);
+//   let $body = $('body');
+//   let isShow = false;
+//   let isStop = true;
+//   /* menu */
 
-  /* slider */
-  let $slides = $('.slide');
-  let $indContainer = $('.indicators');
-  let $indItems = $('.indicator');
-  let currentSlide = 0;
-  let playbackStatus = true;
-  const carouselInterval = 5000;
-  const movedInterval = 500;
-  /* slider */
+//   /* slider */
+//   let $slides = $('.slide');
+//   let $indContainer = $('.indicators');
+//   let $indItems = $('.indicator');
+//   let currentSlide = 0;
+//   let playbackStatus = true;
+//   const carouselInterval = 5000;
+//   const movedInterval = 500;
+//   /* slider */
 
-  /* menu */
-  let toggleActive = () => {
-    $btnHamburger.toggleClass('active');
-    isShow ? $body.removeAttr('class') : $body.toggleClass('active');
-    !isShow && playbackStatus && pauseSlideShow();
-    $navbarList.fadeToggle(500);
-    isShow = !isShow;
-  };
+//   /* menu */
+//   let toggleActive = () => {
+//     $btnHamburger.toggleClass('active');
+//     isShow ? $body.removeAttr('class') : $body.toggleClass('active');
+//     !isShow && playbackStatus && pauseSlideShow();
+//     $navbarList.fadeToggle(500);
+//     isShow = !isShow;
+//   };
 
-  let resetMenu = () => {
-    $btnHamburger.removeClass('active');
-    $body.removeAttr('class');
-    $navbarList.removeAttr('style');
-    isShow = false;
-  };
+//   let resetMenu = () => {
+//     $btnHamburger.removeClass('active');
+//     $body.removeAttr('class');
+//     $navbarList.removeAttr('style');
+//     isShow = false;
+//   };
 
-  $btnHamburger.on('click', () => {
-    toggleActive();
+//   $btnHamburger.on('click', () => {
+//     toggleActive();
 
-    return false;
-  });
+//     return false;
+//   });
 
-  $body.on('click', () => isShow && toggleActive());
+//   $body.on('click', () => isShow && toggleActive());
 
-  $window.on('resize', () => {
-    if ($window.width() > 768 && isStop) {
-      isStop = false;
-      setTimeout(() => {
-        resetMenu();
-        isStop = true;
-      }, 200);
-    }
-  });
-  /* menu */
+//   $window.on('resize', () => {
+//     if ($window.width() > 768 && isStop) {
+//       isStop = false;
+//       setTimeout(() => {
+//         resetMenu();
+//         isStop = true;
+//       }, 200);
+//     }
+//   });
+//   /* menu */
 
-  /* slider */
-  let gotoNSlide = (n) => {
-    const i = currentSlide;
+//   /* slider */
+//   let gotoNSlide = (n) => {
+//     const i = currentSlide;
 
-    $($slides[currentSlide]).toggleClass('active moved');
-    $($indItems[currentSlide]).toggleClass('active');
-    currentSlide = (n + $slides.length) % $slides.length;
-    $($slides[currentSlide]).toggleClass('active');
-    $($indItems[currentSlide]).toggleClass('active');
+//     $($slides[currentSlide]).toggleClass('active moved');
+//     $($indItems[currentSlide]).toggleClass('active');
+//     currentSlide = (n + $slides.length) % $slides.length;
+//     $($slides[currentSlide]).toggleClass('active');
+//     $($indItems[currentSlide]).toggleClass('active');
 
-    setTimeout(() => {
-      $($slides[i]).removeClass('moved');
-    }, movedInterval);
-  };
+//     setTimeout(() => {
+//       $($slides[i]).removeClass('moved');
+//     }, movedInterval);
+//   };
 
-  let gotoNextSlide = () => gotoNSlide(currentSlide + 1);
+//   let gotoNextSlide = () => gotoNSlide(currentSlide + 1);
 
-  let slideInterval = setInterval(gotoNextSlide, carouselInterval);
+//   let slideInterval = setInterval(gotoNextSlide, carouselInterval);
 
-  function pauseSlideShow() {
-    if (playbackStatus) {
-      playbackStatus = !playbackStatus;
-      clearInterval(slideInterval);
-    }
-  }
+//   function pauseSlideShow() {
+//     if (playbackStatus) {
+//       playbackStatus = !playbackStatus;
+//       clearInterval(slideInterval);
+//     }
+//   }
 
-  let clickIndicatorBtn = (e) => {
-    pauseSlideShow();
-    gotoNSlide(+e.target.getAttribute('data-slide-to'));
-  };
+//   let clickIndicatorBtn = (e) => {
+//     pauseSlideShow();
+//     gotoNSlide(+e.target.getAttribute('data-slide-to'));
+//   };
 
-  $indContainer.on('click', '.indicator', clickIndicatorBtn);
-  /* slider */
-});
+//   $indContainer.on('click', '.indicator', clickIndicatorBtn);
+//   /* slider */
+// });
